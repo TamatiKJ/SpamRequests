83dd22336e3ae7633dc0d23d33bceb93beed225b
TamatiKJ <tamati898@gmail.com>
2023-03-03 15:42:55 +0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SpamRequests
-For when you want to spam payment information to a website
+For when you want to spam payment information to a fraudulent website
+payment verification is charged a few cents per request depending on the verification provider
+so spamming payment details that can pass valid-card verifications costs scammers money 
--- spamrequests.py
+++ spamrequests.py
@@ -1,32 +1,40 @@
+# Importing the 'requests' module to send HTTP requests
 import requests
-//adding threading to send multiple requests at once
+# Importing the 'threading' module to add threading support to send multiple requests
 import threading
 
-//the url for the payment processing link
+# URL of the payment processing to send POST requests to
 url = 'http://...'
 
-//pseudo payment information 
-//this card number passes card validity checks
+## Data to send with the POST request
+#Pseudo payment information 
+#This exact card number passes card validity checks
 data = {
     'cc_number' : '4007000000027',
     'cc_expmonth' : '08',
-    'cc_expyear' : '21',
+    'cc_expyear' : '24',
     'cc_cvv' : '234',
 }
 
+# Function that sends POST requests with the specified data, runs infinitely until program is terminated
 def do_request():
     while True:
         response = request.post(url, data=data).text
         print(response)
 
+# Creating a list to hold all the threads
 threads = []
+
+# Creating 50 threads that will execute the 'do_request' function in parallel
 for i in range(50):
     t = threading.Thread(target=do_request)
     t.daemon = True
     threads.append(t)
 
+# Starting all the threads
 for i in range(50):
     threads[i].start()
 
+# Waiting for all the threads to finish executing
 for i in range(50):
     threads[i].join()
